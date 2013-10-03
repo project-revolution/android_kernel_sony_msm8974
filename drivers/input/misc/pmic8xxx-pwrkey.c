9e4c72cf2c65fc209682ea144fbc113e4fdafe8e
krabappel2548 <krabappel2548@gmail.com>
2013-10-03 23:07:09 +0200

    Port dt2wake and sweep2wake: TEST!

--- drivers/input/misc/pmic8xxx-pwrkey.c
+++ drivers/input/misc/pmic8xxx-pwrkey.c
@@ -23,6 +23,10 @@
 #include <linux/mfd/pm8xxx/core.h>
 #include <linux/input/pmic8xxx-pwrkey.h>
 
+#ifdef CONFIG_TOUCHSCREEN_SYNAPTICS_DT2WAKE
+#include <linux/synaptics_i2c_rmi.h>
+#endif 
+
 #define PON_CNTL_1 0x1C
 #define PON_CNTL_PULL_UP BIT(7)
 #define PON_CNTL_TRIG_DELAY_MASK (0x7)
@@ -219,7 +223,12 @@ static int __devinit pmic8xxx_pwrkey_probe(struct platform_device *pdev)
 	pwrkey->pwr = pwr;
 
 	platform_set_drvdata(pdev, pwrkey);
-
+	
+	#ifdef CONFIG_TOUCHSCREEN_SYNAPTICS_DT2WAKE
+		sweep2wake_setdev(pwr); 
+		printk(KERN_INFO "[sweep2wake]: set device %s\n", pwr->name); 
+	#endif 
+	
 	/* check power key status during boot */
 	err = pm8xxx_read_irq_stat(pdev->dev.parent, key_press_irq);
 	if (err < 0) {
